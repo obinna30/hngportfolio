6dd18d3815b8615214147914b13eb53ae1cd7a48
pharmcoder <ekwealorobinna112@gmail.com>
2021-08-17 22:15:28 +0100

    resume done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-
 import Header from "./Components/Header";
-
 
 import "./App.css";
 import About from "./Components/About";
+import { useEffect, useState } from "react";
+import Resume from "./Components/Resume";
 
 const App = () => {
+  const [resumeData, setResumeData] = useState({});
 
-
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <Header  />
-      <About/>
-     
+      <Header />
+      <About />
+      <Resume data={resumeData.resume} />
     </div>
   );
 };
---
+++ src/Components/Resume.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+const Resume = ({ data }) => {
+  if (data) {
+    var skillmessage = data.skillmessage;
+    var education = data.education.map(function (education) {
+      return (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree} <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+          <p>{education.description}</p>
+        </div>
+      );
+    });
+    var work = data.work.map(function (work) {
+      return (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p>{work.description}</p>
+        </div>
+      );
+    });
+    var skills = data.skills.map(function (skills) {
+      var className = "bar-expand " + skills.name.toLowerCase();
+      return (
+        <li key={skills.name}>
+          <span style={{ width: skills.level }} className={className}></span>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <section id="resume">
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">{education}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Work</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">{work}</div>
+      </div>
+
+      <div className="row skill">
+        <div className="three columns header-col">
+          <h1>
+            <span>Skills</span>
+          </h1>
+          
+        </div>
+
+        <div className="nine columns main-col">
+        <div className="skill__images">
+          <img align="left" alt="Visual Studio Code" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" />
+          <img align="left" alt="HTML5" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" />
+          <img align="left" alt="CSS3" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" />
+          <img align="left" alt="Sass" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sass/sass.png" />
+          <img align="left" alt="JavaScript" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" />
+          <img align="left" alt="React" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" />
+          <img align="left" alt="Node.js" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png" />
+          <img align="left" alt="MongoDB" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png" />
+          <img align="left" alt="Git" width="40px" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png" />
+          <img align="left" alt="GitHub" width="40px" src="https://raw.githubusercontent.com/github/explore/78df643247d429f6cc873026c0622819ad797942/topics/github/github.png" />
+          </div>
+
+          <br/>
+          <br/>
+          <br/>
+          
+          <p>{skillmessage}</p>
+
+          <div className="bars">
+            <ul className="skills">{skills}</ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
